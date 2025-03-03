24a5e117fcb04e3875ad0c5683710aa54e3d41db
“brand-point” <the.brandpoint.team@gmail.com>
2025-03-04 02:52:56 +0530

    schema

---
+++ schema/schema.js
@@ -0,0 +1,159 @@
+<!--WebSite-->
+<script type='application/ld+json'>
+{ "@context":"http:\/\/schema.org",
+"@type":"WebSite","@id":"https://brandpoint.in",
+"url":"https:\/\/www.brandpoint.in\/",
+"name":"Brand Point",
+"potentialAction":
+{
+"@type":"SearchAction",
+"target":"https:\/\/www.brandpoint.in\/?s={search_term_string}",
+"query-input":"required name=search_term_string"}
+}
+</script>
+
+
+
+<!-- Brand -->
+<script type="application/ld+json">
+{ "@context" : "http://schema.org",
+  "@type" : "Brand",
+  "name" : "Brand Point",
+  "url": "https://brandpoint.in/",
+  "logo": "https://brandpoint.in/assets/img/brand-point-logo-w-title.png",
+  "sameAs": ["https://brandpoint.in/index.html#products",
+      "https://brandpoint.in/contact.html", "https://brandpoint.in/index.html#our-services", "https://brandpoint.in/blog.html"]  
+}
+</script>
+
+<!-- Organization -->
+
+<script type="application/ld+json">
+{ 
+  "@context": "https://schema.org",
+      "@type": "Organization",
+      "url": "https://brandpoint.in/",
+      "sameAs": ["https://brandpoint.in/index.html#products",
+      "https://brandpoint.in/contact.html", "https://brandpoint.in/index.html#our-services", "https://brandpoint.in/blog.html"],
+      "logo": "https://brandpoint.in/assets/img/brand-point-logo-w-title.png",
+      "name": "Brand Point",
+      "description": "Where Precision Meets Reliability, Welcome to Brand Point, where we transform your vision into a powerful brand identity. Specializing in a wide range of branding solutions, we cater to businesses looking to enhance their presence through customized products and services.",
+      "email": "the.brandpoint.team@gmail.com",
+      "telephone": "+91-93816-55363",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "1-1-293/A, Ashok Nagar Road, Ashok Nagar, Himayatnagar",
+        "addressLocality": "Hyderabad",
+        "addressCountry": "IN",
+        "addressRegion": "Telangana",
+        "postalCode": "500020"
+      }
+    },
+  
+  }
+    </script>
+
+
+<!-- Products -->
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "ProductGroup",
+  "name": "Customizable Products",
+  "hasVariant": [
+    {
+      "@type": "Product",
+      "name": "ID Cards Holders"
+    },
+    {
+      "@type": "Product",
+      "name": "Yoyos"
+    },
+    {
+      "@type": "Product",
+      "name": "Neck Tags"
+    },
+    {
+      "@type": "Product",
+      "name": "Customizable Gift Boxes"
+    },
+    {
+      "@type": "Product",
+      "name": "Customizable Rigid Boxes"
+    },
+    {
+      "@type": "Product",
+      "name": "Customizable Books"
+    },
+    {
+      "@type": "Product",
+      "name": "Customizable Diaries"
+    },
+    {
+      "@type": "Product",
+      "name": "Customizable Calendars"
+    },
+    {
+      "@type": "Product",
+      "name": "Customizable Letterheads"
+    },
+    {
+      "@type": "Product",
+      "name": "Customizable Envelopes"
+    },
+    {
+      "@type": "Product",
+      "name": "Customizable Danglers"
+    },
+    {
+      "@type": "Product",
+      "name": "Customizable Brochures"
+    }
+  ]
+}
+</script>
+
+<!-- Services -->
+
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Service",
+  "serviceType": "Printing and Customization Services",
+  "provider": {
+    "@type": "Organization",
+    "name": "Brand Point",
+    "url": "https://brandpoint.in/"
+  },
+  "hasOfferCatalog": {
+    "@type": "OfferCatalog",
+    "name": "Our Services",
+    "itemListElement": [
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Engraving Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Digital & Offset Printing Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Rubber Prints Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Web Design, Development & Hosting Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable ID Cards Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Stickers Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Business Cards Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Visiting Cards Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Gift Boxes Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Rigid Boxes Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Office Essentials Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Acrylic Trophies Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Crystal Trophies Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Fiber Trophies Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Books Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Diaries Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Calendars Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Letterheads Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Envelopes Printing Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Danglers Printing Services" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Customizable Brochures Printing Services" } }
+    ]
+  }
+}
+</script>
+
+
