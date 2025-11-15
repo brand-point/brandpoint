ca927d062a977de15b9d0171c3001bd949a2c2b2
“brand-point” <srivinayakatravels18oct2025@gmail.com>
2025-11-16 01:55:52 +0530

    float window tagmanager

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,3 +219,11 @@
   });
 
 })();
+
+
+const circle = document.getElementById("whatsappCircle");
+  const popup = document.getElementById("whatsappPopup");
+
+  circle.addEventListener("click", () => {
+    popup.style.display = popup.style.display === "block" ? "none" : "block";
+  });
